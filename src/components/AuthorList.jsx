26ba8cfa37850220f2c4834579d8909870597fc5
herveyyy <captainhervey@gmail.com>
2023-12-06 17:51:49 +0800

    hehe

--- src/components/AuthorList.jsx
+++ src/components/AuthorList.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import AuthorCard from './AuthorCard';
 
-const AuthorList = ({ list, dept}) => {
+const AuthorList = ({ list, dept,profilePic}) => {
   return (
     <div className='h-28 w-80 md:w-[90vh] lg:w-[150vh] overflow-x-auto'>
       <div className='flex gap-x-3'>
         {list.map((author, index) => (
-          <AuthorCard key={index} name={author} dept={dept} />
+          <AuthorCard key={index} name={author} dept={dept} profilePic={profilePic} />
         ))}
       </div>
     </div>
