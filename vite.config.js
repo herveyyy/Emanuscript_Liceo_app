c7f942427f50be31fcf24fe093db938d7d28b511
herveyyy <captainhervey@gmail.com>
2023-12-10 18:22:59 +0800

    gege

--- vite.config.js
+++ vite.config.js
@@ -6,14 +6,35 @@ export default defineConfig({
   plugins: [react(),
   VitePWA({
     manifest:{
-      name: 'Elsa P. Pelaez Memorial Library App',
-        short_name: 'EPPML App',
+      "name": 'Elsa P. Pelaez Memorial Library App',
+        "short_name": 'EPPML App',
+        "display": "standalone",
       icons:[
         {
-          src:"/static/images/libraryLogo.png",
-          sizes:"512x512",
-          type:"image/png",
-        }
+          "src":"/static/images/libraryLogo.png",
+          "sizes":"512x512",
+          "type":"image/png",
+          
+        }, {
+          "src": "/static/images/libraryLogo.png",
+          "sizes": "72x72",
+          "type": "image/png"
+        },
+        {
+          "src": "/static/images/libraryLogo.png",
+          "sizes": "128x128",
+          "type": "image/png"
+        },
+        {
+          "src": "/static/images/libraryLogo.png",
+          "sizes": "144x144",
+          "type": "image/png"
+        },
+        {
+          "src": "/static/images/libraryLogo.png",
+          "sizes": "192x192",
+          "type": "image/png"
+        },
       ]
     }
   })],
